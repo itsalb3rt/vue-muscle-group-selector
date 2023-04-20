4be6ca6850ead510df70e59b0269e69738b591e7
Albert Hidalgo <alhidalgodev@gmail.com>
2023-04-19 21:31:46 -0400

    feat
    * Added assests

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import MuscleGroupSelector from './components/MuscleGroupSelector.vue';
+
+const handleOnSelectMuscularGroup = selection => {
+  console.log(selection) // array of selection e.g ['abs']
+}
 </script>
 
 <template>
-  <div>
+  <div style="max-width: 500px; margin: 50px auto; position: relative;">
     <muscle-group-selector
       :translations="muscularGroupTranslations"
-      @on-select="() => {}"
+      @on-select="handleOnSelectMuscularGroup"
+      :showMusclesListHelper="true"
+      :allowMultiple="true"
     />
   </div>
 </template>
