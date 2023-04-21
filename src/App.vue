268c1d7fdb2e6aa13ee6c651b3042122e2938457
Albert Hidalgo <alhidalgodev@gmail.com>
2023-04-20 23:17:31 -0400

    feat
    * Added example

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <script setup>
 import MuscleGroupSelector from './components/MuscleGroupSelector.vue';
 
-const handleOnSelectMuscularGroup = selection => {
-  console.log(selection) // array of selection e.g ['abs']
-}
+const handleOnSelectMuscularGroup = (selection) => {
+  console.log(selection); // array of selection e.g ['abs']
+};
 </script>
 
 <template>
-  <div style="max-width: 500px; margin: 50px auto; position: relative;">
-    <muscle-group-selector
-      :translations="muscularGroupTranslations"
-      @on-select="handleOnSelectMuscularGroup"
-      :showMusclesListHelper="true"
-      :allowMultiple="true"
-    />
+  <div style="max-width: 500px; margin: 50px auto; position: relative">
+    <div class="row">
+      <div class="col-12">
+        <muscle-group-selector
+          @on-select="handleOnSelectMuscularGroup"
+          :showMusclesListHelper="true"
+          :allowMultiple="true"
+        />
+      </div>
+    </div>
   </div>
 </template>
+<style>
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.col-12 {
+  flex-basis: 0;
+  flex-grow: 1;
+  max-width: 100%;
+}
+
+.col-6 {
+  flex-basis: 0;
+  flex-grow: 1;
+  max-width: 50%;
+}
+</style>
