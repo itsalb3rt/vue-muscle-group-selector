3a1c1f1c8461507e27ef34c82c4bce5a1f5ad5a8
Albert Hidalgo <alhidalgodev@gmail.com>
2023-04-20 22:24:57 -0400

    refactor
    * Removed unused file

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import MuscleGroupSelector from './components/MuscleGroupSelector.vue';
-
-export default {
-  install: (app) => {
-    app.component('MuscleGroupSelector', MuscleGroupSelector);
-  }
-}
